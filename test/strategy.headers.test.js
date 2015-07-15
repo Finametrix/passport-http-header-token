e054947a56f413db7d9db5df0afa0c89c8abffd7
Antonio Bustos <antonio@busrod.net>
2015-07-15 18:36:48 +0200

    validate token with custom 'Negotiate' token title

--- test/strategy.headers.test.js
+++ test/strategy.headers.test.js
@@ -40,4 +40,40 @@ describe('Strategy', function() {
       expect(info.scope).to.equal('read');
     });
   });
+
+  describe('handling a request with valid credentials in headers using custom token name', function() {
+    var options = { tokenTitle: 'Negotiate' };
+    var strategy = new Strategy(options, function(token, done) {
+      if (token === 'a_token') {
+        return done(null, { id: '1234' }, { scope: 'read' });
+      }
+      done(null, false);
+    });
+
+    var user, info;
+
+    before(function(done) {
+      chai.passport(strategy)
+        .success(function(u, i) {
+          user = u;
+          info = i;
+          done();
+        })
+        .req(function(req) {
+          req.headers = {'authorization': 'Negotiate a_token'};
+        })
+        .authenticate();
+    });
+
+    it('should supply user', function() {
+      expect(user).to.be.an.object;
+      expect(user.id).to.equal('1234');
+    });
+
+    it('should supply info', function() {
+      expect(info).to.be.an.object;
+      expect(info.scope).to.equal('read');
+    });
+  });
+
 });
